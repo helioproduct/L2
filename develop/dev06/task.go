014a3cf221e90b2552ed86b72bdb3d9829f53f17
Nikolay Popov <helioproduct.sync@gmail.com>
2024-08-01 13:15:46 +0500

    dev06 added

--- develop/dev06/task.go
+++ develop/dev06/task.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"regexp"
+	"strconv"
+)
+
 /*
 === Утилита cut ===
 
@@ -13,6 +24,296 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
+// cut [OPTION...]
+
+type Splitter interface {
+	Split(b []byte) [][]byte
+}
+
+// Разделяет байты по байтовому сепаратору
+type SplitByteSep struct {
+	sep byte
+}
+
+func NewSplitByteSep(sep byte) *SplitByteSep {
+	return &SplitByteSep{
+		sep: sep,
+	}
+}
+
+func (sb *SplitByteSep) Split(b []byte) [][]byte {
+	var cols [][]byte
+	s := 0
+	for i, e := range b {
+		if e == sb.sep {
+			cols = append(cols, b[s:i])
+			s = i + 1
+		}
+	}
+	cols = append(cols, b[s:])
+
+	return cols
+}
+
+type Filter interface {
+	Filter(cols [][]byte) [][]byte
+}
+
+// Фильтрует заданные колонки
+type FilterSelectedCols struct {
+	selcols [][2]int // [[left right],...]
+	skip    bool
+}
+
+// Создает filter
+func NewFilterSelectedCols(selectedCols string) *FilterSelectedCols {
+	colsParser := NewDefaultListParser()
+
+	return &FilterSelectedCols{
+		selcols: colsParser.Parse(selectedCols),
+	}
+}
+
+type FilterSelectedColsOpts struct {
+	skip bool // не пропускает строки без разделителей
+}
+
+// Создает filter со скипом строк без разделителей
+func NewFilterSelectedColsWithOpts(selectedCols string, opts FilterSelectedColsOpts) *FilterSelectedCols {
+	colsParser := NewDefaultListParser()
+
+	return &FilterSelectedCols{
+		selcols: colsParser.Parse(selectedCols),
+		skip:    opts.skip,
+	}
+}
+
+func (f *FilterSelectedCols) Filter(cols [][]byte) [][]byte {
+	// Если мы не выбираем строчки, то сразу скипаем
+	if len(f.selcols) == 0 {
+		return nil
+	}
+
+	// Если разделителей нет и f.skip (скипнуть такую строчку) - скипаем
+	if f.skip && len(cols) < 2 {
+		return nil
+	}
+	// Если разделителей нет, то пропускаем строчку
+	if len(cols) == 1 {
+		return cols
+	}
+
+	var outCols [][]byte
+	for _, r := range f.selcols {
+		l, r := r[0]-1, r[1] // колонки в промежутке [ )
+		if r == -1 {
+			r = len(cols)
+		}
+
+		// Если некорректный промежуток, то пропускаем его
+		validRange := l >= 0 && l < r && r <= len(cols)
+		if !validRange {
+			continue
+		}
+
+		outCols = append(outCols, cols[l:r]...) // Добавляем промежутки
+	}
+
+	return outCols
+}
+
+// Парсит LIST на последовательность интервалов [[left right],...]
+type ListParser interface {
+	Parse(list string) [][2]int
+}
+
+// Each LIST is made up of one range, or many ranges separated by commas.
+// Selected  input  is written in the same order that it is read, and is written exactly once.  Each range is one
+// of:
+// N      N'th byte, character or field, counted from 1
+// N-     from N'th byte, character or field, to end of line
+// N-M    from N'th to M'th (included) byte, character or field
+// -M     from first to M'th (included) byte, character or field
+type DefaultListParser struct {
+	rer      *regexp.Regexp
+	redigits *regexp.Regexp
+}
+
+func NewDefaultListParser() *DefaultListParser {
+	return &DefaultListParser{
+		rer:      regexp.MustCompile(`(\d+-\d+|\d+-|-?\d+)`), // Находит промежутки
+		redigits: regexp.MustCompile(`\d+`),
+	}
+}
+
+func (p *DefaultListParser) Parse(list string) [][2]int {
+	var selcols [][2]int
+
+	ranges := p.rer.FindAllString(list, -1)
+
+	for _, r := range ranges {
+		digits := p.redigits.FindAllString(r, 2) // Находим числа
+
+		if len(digits) == 2 { // N-M
+			a, err := strconv.Atoi(digits[0])
+			if err != nil {
+				panic(digits[0] + " is not a number!")
+			}
+			b, err := strconv.Atoi(digits[1])
+			if err != nil {
+				panic(digits[1] + " is not a number!")
+			}
+			selcols = append(selcols, [2]int{a, b})
+		} else if r[0] == '-' { // -M
+			b, err := strconv.Atoi(digits[0])
+			if err != nil {
+				panic(digits[0] + " is not a number!")
+			}
+			selcols = append(selcols, [2]int{1, b})
+		} else if r[len(r)-1] == '-' { // N-
+			a, err := strconv.Atoi(digits[0])
+			if err != nil {
+				panic(digits[0] + " is not a number!")
+			}
+			selcols = append(selcols, [2]int{a, -1})
+		} else { // N
+			a, err := strconv.Atoi(digits[0])
+			if err != nil {
+				panic(digits[0] + " is not a number!")
+			}
+			selcols = append(selcols, [2]int{a, a})
+		}
+	}
+
+	return selcols
+}
+
+type Merger interface {
+	Merge(cols [][]byte) []byte
+}
+
+// Сливает колонки с указанным разделителем
+type MergeByteSep struct {
+	sep byte
+}
+
+func NewMergeByteSep(sep byte) *MergeByteSep {
+	return &MergeByteSep{
+		sep: sep,
+	}
+}
+
+func (m *MergeByteSep) Merge(cols [][]byte) []byte {
+	if len(cols) == 0 {
+		return nil
+	}
+
+	var buf bytes.Buffer
+	buf.Grow(len(cols[0]))
+	buf.Write(cols[0])
+
+	for i := 1; i < len(cols); i++ {
+		buf.Grow(len(cols[i]) + 1)
+		buf.WriteByte(m.sep)
+		buf.Write(cols[i])
+	}
+
+	return buf.Bytes()
+}
+
+type Cut struct {
+	splitter Splitter
+	filter   Filter
+	merger   Merger
+}
+
+func NewCut(opts *CutOpts) *Cut {
+	sep := opts.Delimiter
+	selcols := opts.Fields
+
+	return &Cut{
+		splitter: NewSplitByteSep(sep),
+		filter: NewFilterSelectedColsWithOpts(selcols, FilterSelectedColsOpts{
+			skip: opts.Separated,
+		}),
+		merger: NewMergeByteSep(sep),
+	}
+}
+
+func (c *Cut) Cut(input io.Reader, output io.Writer) error {
+	linesep := byte('\n')
+	reader := bufio.NewReader(input)
+
+	var err error
+	linesepBytes := []byte{linesep}
+	for err != io.EOF {
+		var line []byte
+		line, err = reader.ReadBytes(linesep)
+		if err != nil && err != io.EOF {
+			return err
+		}
+		if err != io.EOF {
+			// Удаляем перенос строки
+			line = line[:len(line)-1]
+		}
+		if err == io.EOF && len(line) == 0 {
+			continue
+		}
+
+		sepline := c.splitter.Split(line)
+		filtercols := c.filter.Filter(sepline)
+		if filtercols == nil {
+			continue
+		}
+		mergedline := c.merger.Merge(filtercols)
+
+		if _, err := output.Write(mergedline); err != nil {
+			return err
+		}
+		// Возвращаем перенос строки
+		if _, err := output.Write(linesepBytes); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+type CutOpts struct {
+	Fields    string
+	Delimiter byte
+	Separated bool
+}
+
+func parseArgs() *CutOpts {
+	var cutOpts CutOpts
+	var del string
+
+	flag.StringVar(&cutOpts.Fields, "f", "", "выбрать поля (колонки)")
+	flag.StringVar(&del, "d", "", "использовать другой разделитель")
+	flag.BoolVar(&cutOpts.Separated, "s", false, "только строки с разделителем")
+	flag.Parse()
+
+	if len(del) > 1 {
+		fmt.Fprintln(os.Stderr, "delimiter must be a byte")
+		flag.Usage()
+		os.Exit(2)
+	} else if len(del) == 1 {
+		cutOpts.Delimiter = del[0]
+	} else {
+		cutOpts.Delimiter = '\t'
+	}
+
+	return &cutOpts
+}
+
 func main() {
+	cfg := parseArgs()
+	cut := NewCut(cfg)
+
+	err := cut.Cut(os.Stdin, os.Stdout)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s", err)
+		os.Exit(1)
+	}
 
 }
--- develop/dev06/task_test.go
+++ develop/dev06/task_test.go
@@ -1,1 +1,253 @@
 package main
+
+import (
+	"bytes"
+	"reflect"
+	"testing"
+)
+
+func TestSplitByteSep(t *testing.T) {
+	sep := byte('\t')
+	has := []byte("col1\tcol2\tcol3")
+	want := [][]byte{
+		[]byte("col1"),
+		[]byte("col2"),
+		[]byte("col3"),
+	}
+
+	splitter := SplitByteSep{sep: sep}
+	got := splitter.Split(has)
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("\ngot:  %v\nwant: %v", got, want)
+	}
+}
+
+func TestFilterSelectedCols(t *testing.T) {
+	testCases := []struct {
+		selectCols string
+		has        [][]byte
+		want       [][]byte
+	}{
+		{
+			selectCols: "", // Не выбирать колонок
+			has: [][]byte{
+				[]byte("col1"),
+				[]byte("col2"),
+				[]byte("col3"),
+			},
+			want: nil,
+		},
+		{
+			selectCols: "2", // Выбрать только вторую колонку
+			has: [][]byte{
+				[]byte("col1"),
+				[]byte("col2"),
+				[]byte("col3"),
+			},
+			want: [][]byte{
+				[]byte("col2"),
+			},
+		},
+		{
+			selectCols: "-2", // Выбрать с начала и до второй
+			has: [][]byte{
+				[]byte("col1"),
+				[]byte("col2"),
+				[]byte("col3"),
+			},
+			want: [][]byte{
+				[]byte("col1"),
+				[]byte("col2"),
+			},
+		},
+		{
+			selectCols: "2-", // Выбрать с второй и до конца
+			has: [][]byte{
+				[]byte("col1"),
+				[]byte("col2"),
+				[]byte("col3"),
+			},
+			want: [][]byte{
+				[]byte("col2"),
+				[]byte("col3"),
+			},
+		},
+		{
+			selectCols: "1-3", // С первой по третью
+			has: [][]byte{
+				[]byte("col1"),
+				[]byte("col2"),
+				[]byte("col3"),
+			},
+			want: [][]byte{
+				[]byte("col1"),
+				[]byte("col2"),
+				[]byte("col3"),
+			},
+		},
+		{
+			selectCols: "1, 3, 2-", // Первую, третью, вторую и третью
+			has: [][]byte{
+				[]byte("col1"),
+				[]byte("col2"),
+				[]byte("col3"),
+			},
+			want: [][]byte{
+				[]byte("col1"),
+				[]byte("col3"),
+				[]byte("col2"),
+				[]byte("col3"),
+			},
+		},
+		{ // Если колонок слишком мало -> nil
+			selectCols: "3",
+			has: [][]byte{
+				[]byte("col1"),
+				[]byte("col2"),
+			},
+			want: nil,
+		},
+		{ // Если разделителей нет, то пропускаем
+			selectCols: "3",
+			has: [][]byte{
+				[]byte("line without seps"),
+			},
+			want: [][]byte{
+				[]byte("line without seps"),
+			},
+		},
+		{ // Пустая строка, должна вернутся пустая строка
+			selectCols: "3",
+			has: [][]byte{
+				[]byte(""),
+			},
+			want: [][]byte{
+				[]byte(""),
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		filter := NewFilterSelectedCols(tc.selectCols)
+		got := filter.Filter(tc.has)
+		if !reflect.DeepEqual(got, tc.want) {
+			t.Errorf("\ngot:  %v\nwant: %v", got, tc.want)
+		}
+	}
+}
+
+func TestFilterSelectedColsOpts(t *testing.T) {
+	testCases := []struct {
+		selectCols string
+		skip       bool
+		has        [][]byte
+		want       [][]byte
+	}{
+		{
+			selectCols: "3", // Если разделителей нет, то не пропускаем
+			skip:       true,
+			has: [][]byte{
+				[]byte("line without seps"),
+			},
+			want: nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		filter := NewFilterSelectedColsWithOpts(tc.selectCols, FilterSelectedColsOpts{
+			skip: tc.skip,
+		})
+		got := filter.Filter(tc.has)
+		if !reflect.DeepEqual(got, tc.want) {
+			t.Errorf("\ngot:  %v\nwant: %v", got, tc.want)
+		}
+	}
+}
+
+func TestMergeByteSep(t *testing.T) {
+	sep := byte('\t')
+	has := [][]byte{
+		[]byte("col1"),
+		[]byte("col2"),
+		[]byte("col3"),
+	}
+	want := []byte("col1\tcol2\tcol3")
+
+	merger := MergeByteSep{sep: sep}
+	got := merger.Merge(has)
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("\ngot:  %v\nwant: %v", got, want)
+	}
+}
+
+func TestCut(t *testing.T) {
+	testCases := []struct {
+		desc string
+		opts CutOpts
+		has  []byte
+		want []byte
+	}{
+		{ // just cat
+			desc: "just cat",
+			opts: CutOpts{
+				Fields:    "2",
+				Delimiter: '\t',
+				Separated: false,
+			},
+			has:  []byte("line1\nline2\nline3\n"),
+			want: []byte("line1\nline2\nline3\n"),
+		},
+		{ // just cat with -s
+			desc: "just cat with -s",
+			opts: CutOpts{
+				Fields:    "2",
+				Delimiter: '\t',
+				Separated: true,
+			},
+			has:  []byte("line1\nline2\nline3\n"),
+			want: nil,
+		},
+		{ // print second col
+			desc: "print second col",
+			opts: CutOpts{
+				Fields:    "2",
+				Delimiter: '\t',
+				Separated: false,
+			},
+			has:  []byte("line1\tcol2\nline2\n"),
+			want: []byte("col2\nline2\n"),
+		},
+		{ // print second col with -s
+			desc: "print second col with -s",
+			opts: CutOpts{
+				Fields:    "2",
+				Delimiter: '\t',
+				Separated: true,
+			},
+			has:  []byte("line1\tcol2\nline2\n"),
+			want: []byte("col2\n"),
+		},
+		{ // eof
+			desc: "eof",
+			opts: CutOpts{
+				Fields:    "2",
+				Delimiter: '\t',
+				Separated: false,
+			},
+			has:  []byte("line1"),
+			want: []byte("line1\n"),
+		},
+	}
+
+	for _, tc := range testCases {
+		cut := NewCut(&tc.opts)
+		var got bytes.Buffer
+		err := cut.Cut(bytes.NewBuffer(tc.has), &got)
+		if err != nil {
+			t.Fatalf("err should be nil, but: %s", err)
+		}
+		if !reflect.DeepEqual(got.Bytes(), tc.want) {
+			t.Errorf("desc: %s\ngot:  %v\nwant: %v", tc.desc, got.Bytes(), tc.want)
+		}
+	}
+}
