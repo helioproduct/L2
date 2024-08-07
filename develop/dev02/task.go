263f12d1679331b5bd81d41d6e8929c1541ac1ed
Nikolay Popov <helioproduct.sync@gmail.com>
2024-08-07 14:07:59 +0500

    dev02 updated

--- develop/dev02/task.go
+++ develop/dev02/task.go
@@ -1,64 +1,81 @@
 package main
 
-/*
-=== Задача на распаковку ===
-
-Создать Go функцию, осуществляющую примитивную распаковку строки, содержащую повторяющиеся символы / руны, например:
-	- "a4bc2d5e" => "aaaabccddddde"
-	- "abcd" => "abcd"
-	- "45" => "" (некорректная строка)
-	- "" => ""
-Дополнительное задание: поддержка escape - последовательностей
-	- qwe\4\5 => qwe45 (*)
-	- qwe\45 => qwe44444 (*)
-	- qwe\\5 => qwe\\\\\ (*)
-
-В случае если была передана некорректная строка функция должна возвращать ошибку. Написать unit-тесты.
-
-Функция должна проходить все тесты. Код должен проходить проверки go vet и golint.
-*/
-
 import (
+	"errors"
 	"fmt"
-	// "regexp"
-	"strconv"
 	"strings"
+	"unicode"
 )
 
-func UnpackString(s string) string {
+/*
+Создать Go-функцию, осуществляющую примитивную распаковку строки, содержащую повторяющиеся символы/руны, например:
+"a4bc2d5e" => "aaaabccddddde"
+"abcd" => "abcd"
+"45" => "" (некорректная строка)
+"" => ""
 
-	var count string
-	var rn rune
-	var unpacked string
-	var escape bool
+Дополнительно
+Реализовать поддержку escape-последовательностей.
+Например:
+qwe\4\5 => qwe45 (*)
+qwe\45 => qwe44444 (*)
+qwe\\5 => qwe\\\\\ (*)
 
-	runes := []rune(s + " ")
-	for i := 0; i < len(runes); i++ {
 
-		digit, err := strconv.Atoi(string(runes[i]))
-		if err == nil && !escape {
-			count += strconv.Itoa(digit)
+В случае если была передана некорректная строка, функция должна возвращать ошибку. Написать unit-тесты.
+*/
+
+// Распаковка
+func Unpack(s string) (string, error) {
+	var res strings.Builder
+	var num int         // Сколько раз нужно повторить char
+	var char rune       // Char, который нужно записать
+	var isInitChar bool // Было ли присвоено char какое-то значение
+	var openEscape bool // Нужно ли экранировать символ?
+
+	for _, r := range s {
+
+		if unicode.IsDigit(r) && !openEscape {
+			if !isInitChar { // Если до числа не было символов
+				return "", errors.New("a string cannot start with a number")
+			}
+			num = num*10 + int(r-'0')
+		} else if r == '\\' && !openEscape { // Встречаем escape символ
+			openEscape = true
 		} else {
-			repeatTimes, err := strconv.Atoi(count)
-			if err != nil {
-				repeatTimes = 1
+			if isInitChar {
+				res.WriteRune(char)
+				for ; num > 1; num-- {
+					res.WriteRune(char)
+				}
 			}
-			if rn != 0 {
-				unpacked += strings.Repeat(string(rn), repeatTimes)
-			}
-			rn = runes[i]
-			count = ""
+			num = 0
+			char = r
+			openEscape = false
+			isInitChar = true
+		}
+
+	}
+
+	if openEscape { // При выходе из цикла - последовательность должна быть закрыта
+		return "", errors.New("incomplete escape sequence")
+	}
+
+	if isInitChar {
+		res.WriteRune(char)
+		for ; num > 1; num-- {
+			res.WriteRune(char)
 		}
 	}
-	// fmt.Println(unpacked)
-	return unpacked
+
+	return res.String(), nil
 }
 
 func main() {
-	s := "\t"
-	for _, rn := range []rune(s) {
-		fmt.Printf("%c\n", rn)
+	s := `\02\7`
+	if out, err := Unpack(s); err == nil {
+		fmt.Printf("%s -> %s", s, out)
+	} else {
+		fmt.Println(err)
 	}
-	// UnpackString(s)
-	// fmt.Println([]byte(s))
 }
--- develop/dev02/task_test.go
+++ develop/dev02/task_test.go
@@ -1,1 +1,58 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestUnpack(t *testing.T) {
+	testCases := []struct {
+		s    string
+		want string
+	}{
+		{"", ""},                      // empty string
+		{"abcd", "abcd"},              // without nums
+		{"a4bc2d5e", "aaaabccddddde"}, // one digit nums
+		{"a12", "aaaaaaaaaaaa"},       // big num
+		// escape последовательности
+		{`\02\7`, `007`},       // only digits
+		{`qwe\4\5`, `qwe45`},   // without nums
+		{`qwe\45`, `qwe44444`}, // one digit nums
+		{`\\`, `\`},            // escape escape-rune
+		{`qwe\\5`, `qwe\\\\\`}, // escape escape-rune
+	}
+
+	for _, tc := range testCases {
+		got, err := Unpack(tc.s)
+		if err != nil {
+			t.Fatalf("Err should be nil, but: %s", err.Error())
+		}
+		if got != tc.want {
+			t.Errorf("want \"%s\" got \"%s\"", tc.want, got)
+		}
+	}
+}
+
+func TestErrorUnpack(t *testing.T) {
+	testCases := []struct {
+		s      string
+		errstr string
+	}{
+		{"45", "a string cannot start with a number"}, // just num
+		{`\`, "incomplete escape sequence"},           // incomplete escape
+		{`abc\`, "incomplete escape sequence"},        // incomplete escape
+
+	}
+
+	for _, tc := range testCases {
+		got, err := Unpack(tc.s)
+		if err == nil {
+			t.Fatalf("Err == nil s: %s", tc.s)
+		}
+		if got != "" {
+			t.Fatalf("Got should be \"\" but eq %s; s: %s", got, tc.s)
+		}
+		if err.Error() != tc.errstr {
+			t.Errorf("want \"%s\" got \"%s\"", tc.errstr, err)
+		}
+	}
+}
